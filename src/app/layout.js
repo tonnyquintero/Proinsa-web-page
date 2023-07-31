4013caf43a0e21b8e20774556269f1f982cd1f8e
tonny187 <tonyoanthony@gmail.com>
2023-07-31 15:52:29 -0500

    changing tittle

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Proinsa Clínica Odontológica',
-  description: 'Clínica Odontológica ubicada en Medellin Colombia',
+  description: 'Clínica Odontológica ubicada en San Isidro, Medellin Colombia',
 }
 
 export default function RootLayout({ children }) {
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="es">
       <Head>
         <title>Proinsa Clínica Odontológica</title>
-        <meta name="description" content="Clínica Odontológica ubicada en San Isidro, medellin, Colombia" />
+        <meta name="description" content="Clínica Odontológica ubicada en San Isidro, medellin Colombia" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
