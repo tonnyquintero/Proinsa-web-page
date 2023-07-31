598154eaf34d7d84e0b77a7ef64d9a4f6f5739a2
tonny187 <tonyoanthony@gmail.com>
2023-07-31 10:45:25 -0500

    Changing language

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from 'next/head';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +12,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
+      <Head>
+        <title>Proinsa Clínica Odontológica</title>
+        <meta name="description" content="Clínica Odontológica ubicada en San Isidro, medellin, Colombia" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
